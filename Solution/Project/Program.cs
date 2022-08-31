80711a815fea5b7bedeaedacad1190f06f1679d2
LuizFBR <luizfernando5687@gmail.com>
2022-08-31 20:02:56 -0300

    Outra alteração realizada em Solution/Project/Program.cs

--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Whaa"); //Alteração realizada por Luiz
+Console.WriteLine("Whaa"); //Outra alteração realizada por Luiz
