f4f2cd0f259ba6ddf4d0438a28f27e6ce07769c5
Jarol0709 <jarolbutronsoria@gmail.com>
2022-08-31 20:08:30 -0300

    Outra alteração por Jarol

--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Boa noite!"); // Alteração realizada por Jarol
+Console.WriteLine("Boa dia!"); // Outra alteração realizada por Jarol
 
